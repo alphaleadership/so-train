a019ae6b1d2396a061a524cb5ede2e82cab5196a
alphaleadership <alphaleadership.yt@gmail.com>
2023-09-21 19:09:49 +0200

    init

---
+++ init.cmd
@@ -0,0 +1,7 @@
+echo "# so-train" >> README.md
+git init
+git add */*
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/alphaleadership/so-train.git
+git push -u origin main
