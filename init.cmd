3eeb9592523815fa9e84e1517c04a39a5c4c289e
alphaleadership <alphaleadership.yt@gmail.com>
2023-09-21 19:10:32 +0200

    first commit

--- init.cmd
+++ init.cmd
@@ -1,6 +1,5 @@
-echo "# so-train" >> README.md
-git init
-git add */*
+
+git add init.cmd
 git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/alphaleadership/so-train.git
